d1d112bd709a530d788d3a9cc44fc42d84d06806
Gaurav Thakur <shinra420070186@gmail.com>
2026-08-12 01:00:17 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,10 @@
-const CACHE_NAME = 'crmaster-cache-v1';
+const CACHE_NAME = 'crmaster-cache-v2';
 const ASSETS_TO_CACHE = [
   '/',
   '/index.html',
   '/style.css',
   '/data.js',
+  '/sfx.js',
   '/app.js',
   '/manifest.json'
 ];
